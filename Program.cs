2026496867f1740b8ca971d723086f6b011d80cd
Elijah Rogers <ejrogersemail@gmail.com>
2024-12-03 11:09:15 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Assignment5_2_2
+{
+    internal class Program
+    {
+        //Write a program in C# Sharp to print the first n natural number using recursion.
+        static void Main(string[] args)
+        {
+            RecursiveNaturalNumbers(10);
+        }
+
+        // Each recursive call adds the writeline on a stack until we reach base case (natNums = 1), then it starts poping off the top of the stack
+        static void RecursiveNaturalNumbers(int natNums)
+        {
+            //Console.WriteLine(natNums); // 10->0
+            if (natNums > 0)
+            {
+                //Console.WriteLine(natNums); //10->1
+                RecursiveNaturalNumbers(natNums - 1);
+                Console.WriteLine(natNums); //1->10
+            }
+            //Console.WriteLine(natNums); //0->10
+
+        }
+    }
+}
